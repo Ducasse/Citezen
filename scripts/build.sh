c17b9dfdfed4c6b9fc0b3687a33b67738f6cdd34
StéphaneDucasse <stephane.ducasse@inria.fr>
2024-09-18 22:39:38 +0200

    remove preference loading since it can break

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,7 +30,7 @@ function get_platform_identifier() {
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __builddir="$(pwd)/build"
 PHARO_VERSION="${PHARO_VERSION:-120}"
-PHARO="./pharo Pharo.image"
+PHARO="./pharo Pharo.image --no-default-preferences"
 
 rm -rf "${__builddir}" && mkdir -p "${__builddir}" && cd "${__builddir}"
 wget -O - get.pharo.org/${PHARO_VERSION}+vm | bash
